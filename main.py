89cbd3caeea058a2147b3c9141d8b0190f02c928
JJ Escandor <jj.p.escandor@gmail.com>
2022-07-22 22:20:15 -0400

    Handled error

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from genericpath import exists
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Query, HTTPException
 from typing import Optional
 from pydantic import BaseModel
 
@@ -19,26 +18,31 @@ inventory = {}
 
 @app.get("/get-item/{item_id}")
 def get_item(item_id: int = Path(None, description="The Id of the Item youd like to view",gt=0, lt=10)):
+    if item_id not in inventory:
+        raise HTTPException(status_code=404, detail="Item Id does not exist")
     return inventory[item_id]
+
 
 @app.get("/get-by-name")
 def get_item(*, name: Optional[str] = None):
     for item_id in inventory:
         if inventory[item_id].name == name:
             return inventory[item_id]
-    return {"Data" : "Not Found"}
+    raise HTTPException(status_code=404, detail="Item not found")
+
 
 @app.post("/create-item/{item_id}")
 def create_item(item_id: int, item: Item):
     if item_id in inventory:
-        return {"Error": "Item ID already exists."}
+        raise HTTPException(status_code=404, detail="Item Id already exists")
     inventory[item_id] = item
     return inventory[item_id]
+
 
 @app.put("/update-item/{item_id}")
 def update_item(item_id: int, item: UpdateItem):
     if item_id not in inventory:
-        return {"Error": "Item ID does not exist"}
+        raise HTTPException(status_code=404, detail="Item Id does not exist")
     if item.name != None:
         inventory[item_id].name = item.name
     if item.price != None:
@@ -52,8 +56,7 @@ def update_item(item_id: int, item: UpdateItem):
 @app.delete("/delete-item")
 def delete_item(item_id: int = Query(..., description="The id of the item to delete", gt=0)):
     if item_id not in inventory:
-        return {"Error": "ID does not exisit"}
-
+        raise HTTPException(status_code=404, detail="Item Id does not exist")
     del inventory[item_id]
     return {"Succes": "Item deleted"}
 
